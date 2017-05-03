fd22e472db9e53407756d3d062798677e7692198
zhaokx3 <1329646981@qq.com>
2017-05-03 23:31:58 +0800

    vgg16

--- vgg16.py
+++ vgg16.py
@@ -1,9 +1,7 @@
 ########################################################################################
-# Davi Frossard, 2016                                                                  #
+#zhaokx3, 2017                                                                  #
 # VGG16 implementation in TensorFlow                                                   #
-# Details:                                                                             #
-# http://www.cs.toronto.edu/~frossard/post/vgg16/                                      #
-#                                                                                      #
+
 # Model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md     #
 # Weights from Caffe converted using https://github.com/ethereon/caffe-tensorflow      #
 ########################################################################################
